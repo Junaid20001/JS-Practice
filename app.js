dbebc6338d56eab6e49adbd6dac92ddd1699e4d9
Junaid Khalid <57348907+Junaid20001@users.noreply.github.com>
2022-09-16 17:40:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,585 @@
+// var 
+
+//  var firstName = "Junaid";
+//  var firstName92 = "Khalid";
+
+// document.write(firstName92 );
+// document.write(firstName );
+
+// var  a , b ;
+
+// document.write(a);
+
+// let
+
+// let firstName = "Ali";
+// let firstName = "Baba";
+// document.write(firstName);
+
+// let firstName = "Ali";
+// firstName = "Baba";
+// document.write(firstName);
+
+// const
+// const ali = "Ali";
+// document.write(ali);
+
+// const ali = "Ali";
+// ali = "Baba";                 /*error*/
+// document.write(ali);
+
+
+
+// Data types
+
+// var  x = "Ali";
+// document.write(x);
+// document.write("<br>");
+// document.write(typeof x );
+
+// var  x = 'A';
+// x = 55.945;
+// document.write(x);
+// document.write("<br>");
+// document.write(typeof x );
+
+// var  x = 'A';
+// x = true;
+// document.write(x);
+// document.write("<br>");
+// document.write(typeof x );
+
+// var  x = 'A';
+// x = 55.945;
+// x = true;
+// x = undefined;
+// document.write(x);
+// document.write("<br>");
+// document.write(typeof x );
+
+//  x = ["Ali", "Hasan","Qasim"];
+//  document.write(x);
+//  document.write("<br>");
+//  document.write(typeof x );
+
+// x = {first: "Ali", middle: "Hasan",last:"Qasim"};
+// document.write(x);
+// document.write("<br>");
+// document.write(typeof x );
+
+
+
+// Aritmatic Operator
+
+// var a = 2;
+// var b = 3;
+// var c = a+b;
+// document.write(c);
+ 
+// var a = 6;
+// var b = 3;
+// var c = a - b;
+// document.write(c);
+
+// var a = 6;
+// var b = 3;
+// var c = a * b;
+// document.write(c);
+
+// var a = 6;
+// var b = 3;
+// var c = a / b;
+// document.write(c);
+
+// var a = 6;
+// var b = 3;
+// var c = a % b;
+// document.write(c);
+
+// var a = 3;
+// var b = 2;
+// var c = a ** b;  /*means 3 ki power 2 hai jo k 9 hota hai */
+// document.write(c);
+
+// var a = 6;
+// var b = 3;
+// a++;              /* means a=a+1 yani k a= (a+1) = (6+1) */
+// document.write(a);
+
+
+// var a = 6;
+// var b = 3;
+// document.write(a + b);
+// document.write("<br>");
+// a++;
+// document.write(a+b);
+
+
+// var a = 6;
+// var b = 3;
+// document.write(a + b);
+// document.write("<br>");
+// a--;
+// document.write(a+b);
+
+
+
+// var a = 6;
+// var b = 3;
+// var c = a - b + 3
+// document.write(c);
+
+// Assignment Operator
+
+// a = 5;
+// b = a + 3;
+// console.log(a);
+
+// var a = 7;
+// var b = 2;
+// a += b;
+// document.write(a);
+
+// var a = 7;
+// var b = 2;
+// a -= b;
+// document.write(a);
+
+// var a = 7;
+// var b = 2;
+// a *= b;
+// document.write(a);
+
+// var a = 7;
+// var b = 2;
+// a /= b;
+// document.write(a);
+
+// var a = 7;
+// var b = 2;
+// a %= b;
+// document.write(a);
+
+
+// var a = 4;
+// var b = 3;
+// a **= b;
+// document.write(a);
+
+
+// var x = 4;
+// console.log(x);
+
+// var x = 4;
+// console.table([1,2,3,4,5]);
+
+// var x = 4;
+// console.error("samething error");
+
+// var x = 4;
+// console.clear("samething warning");
+
+// var x = 4;
+// console.time("Timing Hell");
+// console.timeEnd("Timing Hell");
+
+// var ali = "text";
+// document.write(ali);
+
+
+// Comparison Operator
+// var a = 20;
+// var b = 20;
+// console.log( a == b);
+
+// var a = 20;
+// var b = "10";
+// console.log( a == b);
+
+// var a = 20;
+// var b = 20;
+// console.log( a === b);
+ 
+// var a = 20;
+// var b = "20";
+// console.log( a === b);
+
+// var a = 20;
+// var b = "10";  /*data type is not same*/
+// console.log( a === b);
+
+//  var a = 20;
+//  var b = "20";
+//  console.log( a != b);
+
+// var a = 20;
+// var b = 10;
+// console.log( a != b);
+
+
+
+// console.log( 5 != 6);
+
+
+// console.log( 4 != 4);
+
+
+// console.log( 6 !== "6");   /*if one condition is true answer will be false*/ 
+
+
+// console.log( 4 !== 3);
+
+
+// console.log( 4 > 6);
+
+// console.log( 7 > 6);
+
+// console.log( 4 < 6);
+
+
+// console.log( 4 >= 6);
+
+// console.log( 12 >= 6);
+
+// console.log( 6 >= 6);
+
+
+// console.log( 12 <= 6);
+
+// console.log( 12 <= 12);
+
+// console.log( 12 <= 14);
+
+
+// console.log(null == undefined);
+
+// console.log(null === undefined);
+
+// If Statement:
+
+// var a  = 10;
+// var b = 15;
+  
+// if (a > b){
+//     document.write("A is greater");
+// }
+
+// var a  = 100;
+// var b = 15;
+  
+// if (a > b){
+//     document.write("A is greater");
+// }
+
+// var a  = 15;
+// var b = 15;
+  
+// if (a == b){
+//     document.write("A is greater");
+// }
+
+// var a  = 19;
+// var b = 19;
+  
+// if (a === b){
+//     document.write("A is greater");
+// }
+
+
+// var a  = 19;
+// var b = "19";         /*Data type is different*/
+// if (a === b){
+//     document.write("A is greater");
+// }
+
+// var a  = 19;
+// var b = 18;
+// if (a != b){
+//     document.write("A is greater");
+// }
+
+
+// Logical Operator
+// var age = 19;
+// if (age => 18 && age <= 20 ){
+//     console.log("you are eligible");
+// }
+
+// var age = 23;
+// if (age >= 18 && age <= 22 ){
+//     console.log("you are eligible");
+// }
+
+// var a = 19;
+// var b = 22
+// if (a >= 18 || b <= 23 ){
+//     console.log("you are eligible");
+// }
+
+// var a = 17;
+// var b = 22
+// if (a >= 18 || b <= 23 ){
+//     console.log("you are eligible");
+// }
+
+// var a = 30;
+// var b = 25;
+// console.log(! a >= 12);
+
+// var a = 30;
+// var b = 25;
+// console.log(a >= 12);
+
+// var a = 30;
+// var b = 25;
+// console.log(! b >= 26);
+
+
+// if else statement
+// var a = 15;
+// if (a >= 30){console.log("A is Greater");}
+// else console.log("A is smaller");
+
+// var a = 35;
+// if (a >= 30){console.log("A is Greater");}
+// else console.log("A is smaller");
+
+// var a = '30';
+// if (a === 30){console.log("A is same");}
+// else console.log("A is not same");
+
+// var name = 'Junaid';
+// var  sex  = 'male';
+// if (sex == 'male')
+// {console.log("Hello Mr." + name);}
+// else console.log("Hello Mrs."  + name );
+
+// var name = 'Junaid';
+// var  name2 = 'Janiya';
+// var  sex  = 'female';
+// if (sex == 'male')
+// {console.log("Hello Mr." + name);}
+// else {console.log("Hello Mrs."  + name2);}
+
+
+// If  else if statement
+// var per = 3;
+// if (per >= 80 && per <= 100){
+//     document.write("Your in Merit");}
+// else if (per >= 60 && per <= 80){
+//     document.write("Your in Merit 1st Division");}
+// else if (per >= 45 && per <= 60){
+//     document.write("Your in Merit 2nd Division");}
+// else if (per >= 33 && per <= 45){
+//     document.write("Your in Merit 3rd Division");}
+// else if (per < 33){
+//     document.write("You are Fail");
+// }
+// else {
+//     document.write("Enter Valid Percentage");
+// }
+
+
+// Conditional Ternary Operator
+// var a  = 15;
+// var b;
+// (a == 15)? b = "True": b = "False";
+// document.write(b);
+
+// var a  = 15;
+// var b;
+// (a == 20)? b = "True": b = "False";
+// document.write(b);
+
+// var a  = 15;
+// var b;
+// b = (a > 14)? "True": "False";  /* Now Statement will short*/
+// document.write(b);
+
+// var a  = 15;
+// var b;
+// b = "My name  is Junaid & I am " + (a == 15? "True": "False");  /*With message*/
+// document.write(b);
+
+// Switch Case/Statement
+// var day = 01;
+// switch (day) {
+//     case 0:
+//         document.write("Today is Monday");
+//     break;
+
+//     case 1: case 01:      /*mulitple case use in one statement*/
+//         document.write("Today is Tuesday");
+//     break;
+
+//     case 2:
+//         document.write("Today is Wednesday");
+//     break;
+
+//     case 3:
+//         document.write("Today is Thursday");
+//     break;
+
+//     case 4:
+//         document.write("Today is Friday");
+//     break;
+
+//     case 5:
+//         document.write("Today is Saturday");
+//     break;
+
+//     case 6:
+//         document.write("Today is Sundayday");
+//     break;
+
+//     default:
+//         document.write("Enter the valid Day");
+// }
+
+
+//  Alert
+//  var a = 10;
+//  var b = 20;
+//  if  (a > b){alert("A is Greater");}
+//  else {alert("B  is Greater");}
+
+// alert('Welcome to JavaScript');
+
+// var a = confirm("How are you ");
+// if (a){alert("Hello");}
+// else{alert("World");}
+
+// prompt
+// var a = prompt("What is your name");
+// document.write(a);
+// alert(a);
+
+// var per = prompt("Enter your percentege");
+//  if (per >= 80 && per <= 100){
+//      document.write("Your in Merit");}
+//  else if (per >= 60 && per <= 80){
+//      document.write("Your in Merit 1st Division");}
+//  else if (per >= 45 && per <= 60){
+//      document.write("Your in Merit 2nd Division");}
+//  else if (per >= 33 && per <= 45){
+//      document.write("Your in Merit 3rd Division");}
+//  else if (per < 33){
+//      document.write("You are Fail");
+//  }
+//  else {
+//      document.write("Enter Valid Percentage");
+//  }
+
+
+// Functions
+// function Junaid(){
+//     document.write("Junaid Khalid ");
+// }
+// Junaid();
+// document.write("<br>");
+// Junaid();
+// document.write("<br>");
+// Junaid();
+// Junaid();
+
+// function Junaid(){
+//     document.write("Junaid Khalid ");
+// }
+// function Khalid(){
+//     document.write("Junni Khalid ");    /*dual funcation*/
+// }
+
+// Junaid();
+// document.write("<br>");
+// Junaid();
+// Khalid();
+// document.write("<br>");
+// Junaid();
+// Junaid();
+
+
+// Function with Parameters
+// function Junaid(fname, lname){
+//     document.write("Junaid Khalid " + fname + " " + lname);
+// }
+// Junaid("Sardar" , "Khan");
+
+
+// function Junaid(fname, lname){
+//     document.write("Junaid Khalid " + fname + " " + lname);
+// }
+// function Khalid(a , b){
+//     document.write(a  + b);
+// }
+// Junaid("Sardar" , "Khan");
+// Khalid(40,50);
+
+
+// Function return... 
+// function fullname(fname = "Juanid", lname="Khalid"){
+//     var a = fname + " " + lname;
+//     return a;
+// }
+// var fn = fullname("Ayaan","Khan");
+// document.write(fn);
+
+
+// function sum(math,eng,phy){
+//     var s = math + eng + phy;
+//     return s;
+// }
+// var total = sum(65,72,81);
+// document.write(total);
+
+
+// function sum(math,eng,phy){
+//     var s = math + eng + phy;
+//     return s;
+// }
+// function percentage(tt){
+//     var per = tt/300 *100;
+//     document.write(per)
+// }
+// var total = sum(72,72,72);
+// percentage(total);
+
+
+//  function sum(math,eng,phy){
+//      var s = math + eng + phy;
+//      return s;
+//   };
+
+//  var total = sum(72,72,72);
+//      percentage(total);
+//  function percentage(tt){
+//     var c = tt/300 * 100;
+//     return c;
+//  };
+//  var d = percentage(total);
+//  document.write(d + " " +"%");
+
+
+// Global & Local Variable:
+// function hello(){
+//     var a = "Junaid Khalid";        /*Local*/
+//     document.write(a);
+
+// }
+// hello();
+
+// var a = "Junaid Khalid";
+// function hello(){
+//     document.write(a);      /*Global*/
+
+// }
+// hello();
+
+// var a = "Junaid Khalid";   
+// function hello(){
+//     document.write(a);      /*Both Global & Local*/
+
+// }
+// hello();
+// document.write(a);
+
+
